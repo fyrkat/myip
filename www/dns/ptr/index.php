828ba4d477b62ee614202e0b59a0ebf7bcecba5d
Jørn Åne <git@jornane.no>
2016-09-10 12:13:54 +0300

    Add headers to dns/ptr.

--- www/dns/ptr/index.php
+++ www/dns/ptr/index.php
@@ -1,5 +1,9 @@
 <?php require dirname(__DIR__, 3) . implode(DIRECTORY_SEPARATOR, ['', 'src', '_autoload.php']);
 
+$out = gethostbyaddr($_SERVER['REMOTE_ADDR']) . "\r\n";
+
 header('Content-Type: text/plain');
+header('Content-Length: ' . strlen($out));
+header('Access-Control-Allow-Methods: GET');
 header('Access-Control-Allow-Origin: *');
-echo gethostbyaddr($_SERVER['REMOTE_ADDR']) . "\r\n";
+die($out);
